f3c48247a88529a52375fc778cbe63cc4fde6c0b
Alexei <3juigb3@gmail.com>
2024-03-11 14:46:11 +0100

    add 'arbre' class

---
+++ include/Arbre.hpp
@@ -0,0 +1,23 @@
+#include "Roue.hpp"
+
+class Arbre{
+    
+    private:
+    Roue* _menante; //roue menate
+    Roue* _menee; //roue menante
+    double _N; //vitesse de rotation
+    int _sens; //sens de rotation
+
+    public:
+    ~Arbre();
+    Arbre(Roue*, Roue*, double, int);
+    double getN();
+    void setN(double);
+    int getSens();
+    void setSens(int);
+    Roue* getMenee();
+    Roue* getMenante();
+    void setMenante(Roue*);
+    void setMenee(Roue*);
+};
+
--- src/Arbre.cpp
+++ src/Arbre.cpp
@@ -1,1 +1,50 @@
+using namespace std;
+
+#include <cstdlib>
+#include <iostream>
+
 #include "../include/Arbre.hpp"
+#include "../include/Roue.hpp"
+
+Arbre::Arbre(Roue* menee, Roue* menante, double N, int sens){
+    _menee = menee;
+    _menante = menante;
+    _N = N;
+    _sens = sens;
+}
+
+Arbre::~Arbre(){
+    //free(_menee);
+    //free(_menante);
+    _N = 0.;
+    _sens = 0;
+}
+
+double Arbre::getN(){
+    return _N;
+}
+
+void Arbre::setN(double N){
+    _N = N;
+}
+
+int Arbre::getSens(){
+    return _sens;
+}
+
+void Arbre::setSens(int sens){
+    if(sens){
+        _sens = 1;
+    }
+    else{
+        _sens =0;
+    }
+}
+
+Roue* Arbre::getMenante(){
+    return _menante;
+}
+
+Roue* Arbre::getMenee(){
+    return _menee;
+}
